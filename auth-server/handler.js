41c29a6ce9efde4d941a1e9ae63943c1a0861e3b
schen888 <shuang.chen888@hotmail.com>
2022-11-10 16:26:49 +0100

    Add getCalendarEvent function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,18 +1,116 @@
-'use strict';
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+const calendar = google.calendar("v3");
 
-module.exports.hello = async (event) => {
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+const credentials = {
+  client_id: process.env.CLIENT_ID,
+  project_id: process.env.PROJECT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  calendar_id: process.env.CALENDAR_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url:"https://www.googleapis.com/oauth2/v1/certs",
+  redirect_uris:["https://schen888.github.io/meet/"],
+  javascript_origins:["https://schen888.github.io","http://localhost:3000"],
+};
+const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
+const oAuth2Client = new OAuth2(client_id,client_secret,redirect_uris[0]);
+
+module.exports.getAuthURL = async () => {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({authUrl: authUrl})
+  };
+};
+
+module.exports.getAccessToken = async (event) => {
+  
+    const oAuth2Client = new OAuth2(client_id,client_secret,redirect_uris[0]);
+
+    const code = decodeURIComponent(`${event.pathParameters.code}`);
+  
+    return new Promise((resolve, reject) => {
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(token);
+      });
+    })
+      .then(token => {
+        return {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*"
+          },
+          body: JSON.stringify(token)
+        };
+      })
+      .catch((err) => {   
+        console.error(err);
+        return {
+          statusCode: 500,
+          headers: {
+            "Access-Control-Allow-Origin": "*"
+          },
+          body: JSON.stringify(err)
+        };
+      });
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  module.exports.getCalendarEvents = async (event) => {
+    const oAuth2Client = new OAuth2(client_id,client_secret,redirect_uris[0]);
+
+    const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+
+    oAuth2Client.setCredentials({ access_token });
+
+
+    return new Promise((resolve, reject)=>{
+      calendar.events.list(
+        {
+          calendarId: calendar_id,
+          auth: oAuth2Client,
+          timeMin: new Date().toISOString(),
+          singleEvents: true,
+          orderBy: "startTime",
+        },
+        (error, response) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(response);
+          }
+        }
+      );
+    })
+    .then ((results)=>{
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({ events: results.data.items })
+      }
+    })
+    .catch((err) => {   
+      console.error(err);
+      return {
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(err)
+      };
+    });
+  }
