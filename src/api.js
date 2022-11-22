c53994c964d9099193f7717ee8f1f2f1a7e75540
schen888 <shuang.chen888@hotmail.com>
2022-11-22 14:13:08 +0100

    Add feature to load cached lastEvents when offline.

--- src/api.js
+++ src/api.js
@@ -42,6 +42,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data ? JSON.parse(data).events : [];
+  }
+
   const token = await getAccessToken();
   if (token) {
     removeQuery();
